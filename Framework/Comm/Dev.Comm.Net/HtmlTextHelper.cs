9b679e1cc2106f7b7ec9aac7eafd9a3e8bea4caf
zbw911 <zbw911@qq.com>
2014-10-28 09:32:00 +0800

    Add HtmltestHelper

---
+++ Framework/Comm/Dev.Comm.Net/HtmlTextHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Dev.Comm.Net
+{
+    /// <summary>
+    /// 对返回的Html进行分析的帮助方法
+    /// </summary>
+    public class HtmlTextHelper
+    {
+        /// <summary>
+        /// 使正则表达式取得Input的Value
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="inputname"></param>
+        /// <returns></returns>
+        public static string FindValueByName(string str, string inputname)
+        {
+            string reg = @"<input [\s\S]*? name=""(?<name>.*?)"" [\s\S]*?value=""(?<value>.*?)"" [\s\S]*?>";
+            Regex r = new Regex(reg, RegexOptions.None);
+            Match match = r.Match(str);
+            string aa = "";
+            while (match.Success)
+            {
+                string name = match.Groups["name"].ToString();
+                string value = match.Groups["value"].ToString();
+                if (name == inputname)
+                {
+                    return value;
+                }
+                else
+                {
+                    match = match.NextMatch();
+                }
+            }
+            return aa;
+        }
+    }
+}
---
+++ Framework/Comm/Dev.Comm.Test/NET/HtmlTextHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Dev.Comm.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev.Comm.Test.NET
+{
+    [TestClass]
+    public class HtmlTextHelperTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = @"<form name='form' action='Radioivote.asp' method='Post'>
+<input type='Hidden' name='Voeid' id='VoTeid' value='49a57a54a49' />
+<input type='Hidden' name='VotId' id='VotId' value='3779' /></form>";
+
+
+            var v = HtmlTextHelper.FindValueByName(test.Replace('\'', '"'), "Voeid");
+
+            Assert.AreEqual("49a57a54a49", v);
+        }
+    }
+}
